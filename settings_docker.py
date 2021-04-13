ee742e5cef707dd34386c7ac4e5ce1c9a1eefc80
c64bob <c64bob@users.noreply.github.com>
2021-04-13 13:37:37 +0200

    Update settings_docker.py

--- settings_docker.py
+++ settings_docker.py
@@ -19,38 +19,5 @@ DEFAULT_REPO_STORAGE = [
     (os.path.join(DATA_DIR, 'repos'), 'https://%s/repos/' % os.getenv('REPOMAKER_HOSTNAME')),
 ]
 
-
-#LOGIN_REDIRECT_URL = "/"
-# http://django-allauth.readthedocs.io/en/latest/installation.html
-#INSTALLED_APPS += [
-#    'allauth',
-    # 'allauth.socialaccount',
-    # 'allauth.socialaccount.providers.amazon',
-    # 'allauth.socialaccount.providers.baidu',
-    # 'allauth.socialaccount.providers.bitbucket_oauth2',
-    # 'allauth.socialaccount.providers.dropbox_oauth2',
-    # 'allauth.socialaccount.providers.facebook',
-    # 'allauth.socialaccount.providers.github',
-    # 'allauth.socialaccount.providers.gitlab',
-    # 'allauth.socialaccount.providers.google',
-    # 'allauth.socialaccount.providers.linkedin_oauth2',
-    # 'allauth.socialaccount.providers.openid',
-    # 'allauth.socialaccount.providers.reddit',
-    # 'allauth.socialaccount.providers.slack',
-    # 'allauth.socialaccount.providers.stackexchange',
-    # 'allauth.socialaccount.providers.twitter',
-    # 'allauth.socialaccount.providers.weibo',
-#]
-#AUTHENTICATION_BACKENDS = (
-#    'django.contrib.auth.backends.ModelBackend',
-#    'allauth.account.auth_backends.AuthenticationBackend',
-#)
-#ACCOUNT_FORMS = {
-#    'login': 'repomaker.views.RmLoginForm',
-#    'reset_password': 'repomaker.views.RmResetPasswordForm',
-#    'signup': 'repomaker.views.RmSignupForm',
-#}
-#ACCOUNT_EMAIL_VERIFICATION = "none"
-
 if DEBUG:
     SASS_PROCESSOR_ENABLED = False
